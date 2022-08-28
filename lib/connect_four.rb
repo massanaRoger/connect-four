804aa135c1afb4345e649af705fa91d5e0c9f6b7
Roger Massana <rogermassana2002@gmail.com>
2022-08-28 16:34:48 +0200

    Do #play without checking if winner

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -6,6 +6,7 @@ class Game
 
   def initialize
     fill_board
+    @use_x = true
   end
 
   def fill_board
@@ -30,19 +31,40 @@ class Game
   end
 
   def square_contain_win?(row, col, piece)
-    mask = {
-      right_diag: [[row, col], [row + 1, col + 1], [row + 2, col + 2], [row + 3, col + 3]],
-      left_diag: [[row, col], [row + 1, col - 1], [row + 2, col - 2], [row + 3, col - 3]],
-      right: [[row, col], [row, col + 1], [row, col + 2], [row, col + 3]],
-      down: [[row, col], [row + 1, col], [row + 2, col], [row + 3, col]]
-    }
+    mask = declare_mask(row, col)
+    !mask.filter { |_key, value| row_length(value, piece) == 4 }.empty?
+  end
 
-    !mask.filter { |key, value| row_length(value, piece) == 4 }.empty?
+  def play
+    piece = @use_x ? 'X' : 'Y'
+    col = get_column
+    insert_piece(col, piece)
+    @use_x = @use_x ? false : true
   end
 
   private
 
   def row_length(arr, piece)
     arr.filter { |pos| @board[pos[0]][pos[1]] == piece }.length
+  end
+
+  def declare_mask(row, col)
+    {
+      right_diag: [[row, col], [row + 1, col + 1], [row + 2, col + 2], [row + 3, col + 3]],
+      left_diag: [[row, col], [row + 1, col - 1], [row + 2, col - 2], [row + 3, col - 3]],
+      right: [[row, col], [row, col + 1], [row, col + 2], [row, col + 3]],
+      down: [[row, col], [row + 1, col], [row + 2, col], [row + 3, col]]
+    }
+  end
+
+  def get_column
+    number = gets.chomp
+    number.numeric? ? number.to_i : nil
+  end
+end
+
+class String
+  def numeric?
+    Float(self) != nil rescue false
   end
 end
