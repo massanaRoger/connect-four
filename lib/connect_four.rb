b31b7ea590a1d81b3cf5f4fd67f9807c96e9226a
Roger Massana <rogermassana2002@gmail.com>
2022-08-28 15:13:32 +0200

    Do #square_contain_win? and testing

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -28,4 +28,17 @@ class Game
   def exist_piece?(pos_x, pos_y)
     @board[pos_x][pos_y] != '%'
   end
+
+  def square_contain_win?(i, j, piece)
+    right_diag = [[i,j], [i+1,j+1], [i+2,j+2], [i+3,j+3]]
+    left_diag = [[i,j], [i-1,j+1], [i-2,j+2], [i-3,j+3]]
+    right = [[i,j], [i,j+1], [i,j+2], [i,j+3]]
+    down = [[i,j], [i+1,j], [i+2,j], [i+3,j]]
+    right_diag = right_diag.filter { |pos| @board[pos[1]][pos[0]] == piece }
+    left_diag = left_diag.filter { |pos| @board[pos[1]][pos[0]] == piece }
+    right = right.filter { |pos| p pos; @board[pos[1]][pos[0]] == piece }
+    down = down.filter { |pos| @board[pos[1]][pos[0]] == piece }
+    p board
+    right_diag.length == 4 || left_diag.length == 4 || right.length == 4 || down.length == 4
+  end
 end
